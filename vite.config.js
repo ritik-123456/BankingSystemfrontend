57b576e16cb5dadfb2a660f61926b9cfa8504313
Ritik Kumar <ritikkr274@gmail.com>
2026-07-03 10:27:48 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://bankingsystem-backend-v51o.vercel.app',
         changeOrigin: true,
         secure: false
       }
